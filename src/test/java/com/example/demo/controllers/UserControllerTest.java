355d82eba5d4aee65e3aea13a89ab4f1f6ddbbc5
Wang, John (cw457c) <cw457c@us.att.com>
2021-03-23 15:19:48 -0700

    first commit

---
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,57 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+    private UserController userController;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private CartRepository cartRepository = mock(CartRepository.class);
+    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+//    private UserRepository userRepository;
+//    private CartRepository cartRepository;
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Before
+    public void setUp(){
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository", userRepository);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder", encoder);
+
+    }
+
+    @Test
+    public void create_user_happy_path(){
+        when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
+        CreateUserRequest req = new CreateUserRequest();
+        req.setUsername("test");
+        req.setPassword("testPassword");
+        req.setConfirmPassword("testPassword");
+        final ResponseEntity<User> resp = userController.createUser(req);
+        assertNotNull(resp);
+        assertEquals(200, resp.getStatusCodeValue());
+
+        User user = resp.getBody();
+        assertNotNull(user);
+        assertEquals(0, user.getId());
+        assertEquals("test", user.getUsername());
+        assertEquals("thisIsHashed", user.getPassword());
+    }
+
+}
